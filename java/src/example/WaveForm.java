a10701f6b71c6f337f3f54583064e00e646c54af
Luis Pérez <c22734141@mytudublin.ie>
2024-03-23 21:33:01 +0000

    Change 1

--- java/src/example/WaveForm.java
+++ java/src/example/WaveForm.java
@@ -3,29 +3,22 @@ package example;
 import processing.core.*;
 
 // This is an example of a visual that renders the waveform
-public class WaveForm
-{
+public class WaveForm {
     MyVisual mv;
     float cy = 0;
 
-    public WaveForm(MyVisual mv)
-    {
+    public WaveForm(MyVisual mv) {
         this.mv = mv;
         cy = this.mv.height / 2;
     }
 
-    public void render()
-    {
+    public void render() {
         mv.colorMode(PApplet.HSB);
-        for(int i = 0 ; i < mv.getAudioBuffer().size() ; i ++)
-        {
+        for (int i = 0; i < mv.getAudioBuffer().size(); i++) {
             mv.stroke(
-                PApplet.map(i, 0, mv.getAudioBuffer().size(), 0, 255)
-                , 255
-                , 255
-            );
+                    PApplet.map(i, 0, mv.getAudioBuffer().size(), 0, 255), 255, 255);
 
-            mv.line(i, cy, i, cy + cy * mv.getAudioBuffer().get(i));
+            mv.line(i, cy, i, cy + cy * mv.getAudioBuffer().get(i) * 10); // Added * 10
         }
     }
 }
